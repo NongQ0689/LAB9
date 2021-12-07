8da962a684c7dfca3d3ad968f8e670a84e47a8af
NongQ0689 <suraphat.q@gmail.com>
2021-12-07 16:46:31 +0700

    Finish

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -9,3 +9,39 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter your age: ";
+    int age ; cin>>age;
+    if (age<=20)
+    {
+        cout<<"Enter your height: ";
+        int hight ; cin>>hight;
+        if(hight<160) 
+            cout<<"Your status = UNFRIEND";
+        else if(hight<175) 
+            cout<<"Your status = FRIEND";
+        else
+        {
+            cout<<"Enter your property: ";
+            int pro ; cin>>pro;
+            if(pro>69000000)
+                 cout<<"Your status = MARRIED";
+            else cout<<"Your status = ONE-NIGHT-STAND";
+        }
+    }else if (age<30)
+    {
+        cout<<"Enter your property: ";
+        int pro ; cin>>pro;
+        if(pro>10000000)
+             cout<<"Your status = BEST FRIEND";
+        else cout<<"Your status = UNFRIEND";
+
+    }else cout<<"Your status = UNFRIEND";
+
+}
